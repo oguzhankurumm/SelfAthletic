e7cd21f18bce30a1c61664a555c659a0a53d33dd
Oğuzhan Kurum <59623554+oguzhankurumm@users.noreply.github.com>
2021-12-20 15:38:53 +0300

    edited slider details

---
+++ src/screens/workoutspages/SliderDetails/index.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, StatusBar, Image, SafeAreaView, ScrollView, Alert, TouchableOpacity, ImageBackground, Dimensions, Linking } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon2 from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import SpinnerLoading from '../../components/SpinnerLoading';
+import { useSelector } from 'react-redux';
+import { database } from '../../config/config';
+import moment from 'moment';
+import styles from './style';
+import ImageLayout from '../../../components/image-layout';
+
+const SliderDetails = props => {
+    const profileData = useSelector(state => state.authReducer.currentUser);
+
+    const [Loading, setLoading] = useState(false);
+    const [Details, setDetails] = useState(props.route.params.item);
+
+    useEffect(() => {
+        if (Details.length !== 0) {
+            setLoading(false);
+        } else {
+            setLoading(true);
+        }
+    }, [])
+
+    const SocialCard = ({ url, icon }) => (
+        <TouchableOpacity onPress={() => {
+            Linking.canOpenURL(url).then(supported => {
+                if (supported) {
+                    Linking.openURL(url);
+                } else {
+                    console.log("Don't know how to open URI: " + url);
+                }
+            });
+        }} style={styles.socialButton}>
+            <Icon name={icon} color="#FFF" size={22} />
+            <Text style={styles.socialText}>Takip Et</Text>
+        </TouchableOpacity>
+    )
+
+    return (
+        <ImageLayout
+            title={Details.title}
+            Loading={Loading}
+            showBack
+            isScrollable={true}
+        >
+            {!Loading &&
+                <View style={styles.imageContainer}>
+                    <Image
+                        resizeMode="cover"
+                        source={{ uri: Details.image }}
+                        style={styles.image}
+                    />
+
+                    <LinearGradient
+                        start={{ x: 1, y: 1 }}
+                        end={{ x: 1, y: 1 }}
+                        colors={['rgba(0,0,0,0.6)', 'transparent']}
+                        style={styles.linearGradient}
+                    />
+
+                    <View style={{ position: 'absolute', top: 15, paddingHorizontal: 20 }}>
+                        <Text style={styles.title}>{Details.title}</Text>
+                    </View>
+                    <View style={{ marginTop: 20 }}>
+                        <Text style={styles.description}>{Details.description}</Text>
+                    </View>
+
+                    {Details.showSocials === true &&
+                        <View style={{ flexDirection: 'row', width: '100%', marginTop: 20, justifyContent: 'space-between' }}>
+                            <SocialCard url='https://www.instagram.com/selfathletic/' icon='social-instagram' />
+                            <SocialCard url='https://www.twitter.com/selfathletic/' icon='social-twitter' />
+                            <SocialCard url='https://www.facebook.com/selfathletic/' icon='social-facebook' />
+                        </View>
+                    }
+                </View>
+            }
+        </ImageLayout>
+    )
+}
+
+
+export default SliderDetails;
---
+++ src/screens/workoutspages/SliderDetails/style.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        paddingHorizontal: 20
+    },
+    imageContainer: {
+        height: 'auto',
+        width: '100%',
+        borderRadius: 18,
+        marginTop: 10
+    },
+    image: {
+        width: '100%',
+        minHeight: 200,
+        height: 'auto',
+        borderRadius: 18
+    },
+    socialButton: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#202026',
+        padding: 10,
+        borderRadius: 12
+    },
+    socialText: {
+        marginLeft: 10,
+        fontFamily: 'SFProDisplay-Bold',
+        justifyContent: 'flex-start',
+        fontSize: 14,
+        color: '#FFF'
+    },
+    linearGradient: {
+        position: 'absolute',
+        borderRadius: 18,
+        width: '100%',
+        minHeight: 200,
+        height: 'auto'
+    },
+    title: {
+        fontFamily: 'SFProDisplay-Bold',
+        fontSize: 20,
+        color: '#FFF',
+        marginBottom: 8
+    },
+    description: {
+        fontFamily: 'SFProDisplay-Medium',
+        fontSize: 13,
+        color: '#FFF'
+    }
+})
+
+export default styles;
