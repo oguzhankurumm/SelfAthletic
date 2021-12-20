e33b93d6a738dd2a46417f2a210c32fc890cd322
Oğuzhan Kurum <59623554+oguzhankurumm@users.noreply.github.com>
2021-12-20 15:30:17 +0300

    changed workout thumbnail page

---
+++ src/screens/workoutspages/MoveThumb/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const MoveThumb = () => {
+    return (
+        <View>
+            <Text>Hello from movement thumbnail</Text>
+        </View>
+    )
+}
+
+export default MoveThumb;
